f776393364fd6e1bfa91d82e56cb12c54f14f80f
Aidan Marian <Gameleader2007@gmail.com>
2025-05-23 15:20:03 -0700

    hey check this out?

--- engine/Main.java
+++ engine/Main.java
@@ -9,19 +9,7 @@ public class Main
     System.out.println("great success");
     // create a new map object
     Map map = new Map();
-    Tile[][] testing = map.getField();
-    for (int row = 0; row < map.getField().length; row++) {
-      for (int col = 0; col < map.getField()[0].length; col++) {
-        if (testing[row][col] instanceof HardWall) {
-          System.out.print("H");
-        } else if (testing[row][col] instanceof SoftWall) {
-          System.out.print("S");
-        } else {
-          System.out.print(" ");
-        }
-        System.out.print(" ");
-        }
-        System.out.println();
-      }
+    map.printMap();
+
     }
   }   

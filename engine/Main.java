a30e4fcd24a6b020d07518b2c5493b7c7744ef92
Aidan Marian <Gameleader2007@gmail.com>
2025-05-23 15:15:19 -0700

    Testing

--- engine/Main.java
+++ engine/Main.java
@@ -6,6 +6,22 @@ public class Main
   {
     // create a new game window
     GameWindow window = new GameWindow();
-    
+    System.out.println("great success");
+    // create a new map object
+    Map map = new Map();
+    Tile[][] testing = map.getField();
+    for (int row = 0; row < map.getField().length; row++) {
+      for (int col = 0; col < map.getField()[0].length; col++) {
+        if (testing[row][col] instanceof HardWall) {
+          System.out.print("H");
+        } else if (testing[row][col] instanceof SoftWall) {
+          System.out.print("S");
+        } else {
+          System.out.print(" ");
+        }
+        System.out.print(" ");
+        }
+        System.out.println();
+      }
+    }
   }   
-}
